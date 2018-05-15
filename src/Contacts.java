b5c3624d26e17e0315f7f2e2d03aebb3806ea7b8
Kelly Villalobos <kvdesign1978@gmail.com>
2018-05-15 14:31:05 -0500

    made Contact class with private name and number and name and number constructor

--- src/Contacts.java
+++ src/Contacts.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+
 public class Contacts {
+    public static void main(String[] args) {
+
+    }
+    private ArrayList<String> names;
+    private ArrayList<String> number;
+
+    //-------constructor-------------
+    public Contacts(ArrayList<String> name, ArrayList<String> number){
+        this.names = name;
+        this.number = number;
+    }
 }
